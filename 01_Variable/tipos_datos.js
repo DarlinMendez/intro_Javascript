59337013259b7ddcb3e253a5c26268396d1761a3
Darlin <darlinklk22@gmail.com>
2020-04-26 22:31:14 -0400

    Reglas Basicas de Js, Variables,Constantes y tipos de datos primitivos.

---
+++ 01_Variable/tipos_datos.js
@@ -0,0 +1,23 @@
+// Texto = String
+let nombreCompleto = "Cristian";
+let apellido = "nunez";
+let pais = "Republica Dominicana";
+
+
+// Numero = Number
+let edad = 23;
+let year = 2020;
+let millon = 1000000;
+const PI = 3.14;
+let precioDollar = 54.8;
+
+// Logicos = Boolean
+
+let tieneCarro = false;
+let posseePC = true;
+
+
+console.log(nombreCompleto);
+console.log(edad);
+console.log(tieneCarro)
+console.log(typeof tieneCarro)
---
+++ 01_Variable/variables.js
@@ -0,0 +1,8 @@
+let deuda = 1990;  
+let nombre = "Santiago";
+let fechaNacimiento = "10/1/200";
+
+console.log("Tu deuda es de 0 " + deuda);
+console.log("Bienvenido a tu Pagina " + nombre);
+console.log("Naciste en la fecha "  + fechaNacimiento);
+
