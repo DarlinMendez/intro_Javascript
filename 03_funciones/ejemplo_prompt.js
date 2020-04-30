1ddb63db7bd094f66ffeb56e0106b5fa5e04660c
Darlin <darlinklk22@gmail.com>
2020-04-30 10:26:36 -0400

    Ejercicios Completados

--- 03_funciones/ejemplo_prompt.js
+++ 03_funciones/ejemplo_prompt.js
@@ -1,4 +1,4 @@
- //let nombre = prompt("Dime tu nombre");
+/* //let nombre = prompt("Dime tu nombre");
  //let edad = prompt("Dime tu edad");
  //let EstadooCivil = prompt("Dime tu estado Civil");
 
@@ -27,3 +27,21 @@ console.log(estudiante.edad);
 estudiante.edad = Number.parseInt(estudiante.edad);
 
 console.log(estudiante.edad);
+*/
+
+
+let nombre = prompt("Como te llamas?");
+let edad = Number.parseInt (prompt("Que edad tienes?"));
+let monto = Number.parseInt( prompt("Que cantidad tienes en pesos?") );
+
+console.log(typeof monto);
+
+//convertir edad en numero
+edad = Number.parseInt(edad);
+
+let tasaDelDolar = 54.3
+let conversionEnDollar = monto / tasaDelDolar
+
+//console.log(`la conversionn es: ${conversionEnDollar.toFixed(2)}`);
+
+console.log(`haz digitado ${monto} pesos. la tasa del dollar es: ${tasaDelDolar} la conversion es ${conversionEnDollar.toFixed(2)}` );
